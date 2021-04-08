65b015a1aca0739a738ba31df6adc593cd3ea7cc
azufb <curiousazunas@gmail.com>
2021-04-08 18:41:27 +0900

    カードコンポーネント試作

--- src/component/Works.js
+++ src/component/Works.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    NavLink
+} from 'react-router-dom';
+import todo from './../reacttodoappagain.png';
 
 export default class Works extends React.Component {
     render() {
         return (
             <div>
                 <h2>成果物</h2>
+                <h3>タスク管理</h3>
+                <div className='cards'>
+                    <div className='card'>
+                        <img src={todo} />
+                        <p>ToDo管理</p>
+                    </div>
+                    <div className='card'>
+                        <img src={todo} />
+                        <p>ToDo管理</p>
+                    </div>
+                </div>
             </div>
         );
     }
