a48283124dae5db5c7ea1515f067187c7eba9b9b
azufb <curiousazunas@gmail.com>
2021-04-09 23:07:04 +0900

    個別ページとリンク付け(エンタメ・就活)

---
+++ src/component/works/negapuchi.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default class Negapuchi extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>ネガプチッ！</h2>
+                <p>
+                    このアプリケーションは、ネガティブな表現を、緩和するアプリです。
+                </p>
+
+                <h3>作成したきっかけ</h3>
+                <p>
+                    やりとりをしていて傷つくことが多々あったから。
+                </p>
+
+                <h3>使用技術</h3>
+                React
+            </div>
+        )
+    }
+}
